06493c67bfa2e8abb85f2673a34276193e84fbba
Jonas Jensen <jdudal11@gmail.com>
2018-01-28 18:03:26 +0100

    Added player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public int movementSpeed;
+
+	Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+		//Movment calculations
+		rb.velocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rb.velocity.y);
+
+		//Attack
+
+	}
+}
