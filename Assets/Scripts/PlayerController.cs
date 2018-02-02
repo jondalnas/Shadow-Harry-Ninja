49615fca7ce5921f777cfdf196d36855464c0fc0
Jondalnas <jdudal11@gmail.com>
2018-02-02 12:26:34 +0100

    Player animation when walking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	public int movementSpeed;
+	public float animationDeadZone = 0.2f;
 
 	Rigidbody2D rb;
+	Animator anim;
 
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
 	}
-	
-	void Update () {
-		//Movment calculations
-		rb.velocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rb.velocity.y);
 
+	private void Update() {
 		//Attack
 
+	}
+
+	void FixedUpdate() {
+		//Movment calculations
+		rb.velocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rb.velocity.y);
+		if (Mathf.Abs(rb.velocity.x) > animationDeadZone) {
+			anim.SetBool("Walking", true);
+
+			if (rb.velocity.x > 0)
+				anim.SetFloat("Speed", 1);
+			else
+				anim.SetFloat("Speed", -1);
+		} else
+			anim.SetBool("Walking", false);
 	}
 }
